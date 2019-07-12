ff750d59666fcf9334330571b89af3a5157cb7f2
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2019-07-12 09:15:03 +0300

    chore(docker): update docker build
    Update Docker build command to build avatar bot image

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build $DOCKER_USERNAME/avatar-bot
+docker build -t $DOCKER_USERNAME/avatar-bot .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_USERNAME/avatar-bot
